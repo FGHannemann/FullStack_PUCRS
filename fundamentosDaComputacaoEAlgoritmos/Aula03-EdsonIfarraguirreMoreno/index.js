edfa280396ac87c733d452a30ea10878539775cb
Fernando Hannemann <hannemann.pa@gmail.com>
2024-03-27 22:01:59 -0300

    Continuando a Aula03-EdsonIfarraguirreMoreno

--- fundamentosDaComputacaoEAlgoritmos/Aula03-EdsonIfarraguirreMoreno/index.js
+++ fundamentosDaComputacaoEAlgoritmos/Aula03-EdsonIfarraguirreMoreno/index.js
@@ -1,2 +1,21 @@
+/*
+** Conceito inicial
+*/
 console.log('Hello World!!!')
 console.log('Fundamentos de Computação e Algoritmos.')
+
+
+/*
+** Implementando a primeira função
+*/
+function parImpar(limitSup){
+    for (let i=0;  i<limitSup; i++){
+        if(i%2==1)
+            console.log(i+' é um número ímpar!')
+        else
+            console.log(i+' é um número par!')
+    }
+}
+
+parImpar(10)
+
